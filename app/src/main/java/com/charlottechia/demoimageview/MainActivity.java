b3cf2e2ea60f85252c680785e1a1fd6249fa8c55
17010582 <17010582@myrp.edu.sg>
2019-04-30 10:15:04 +0800

    Initial commit

---
+++ app/src/main/java/com/charlottechia/demoimageview/MainActivity.java
@@ -0,0 +1,15 @@
+package com.charlottechia.demoimageview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        
+    }
+}
